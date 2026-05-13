97024617cd77fbbfbbd54397a3a2ac0c00db0086
Vanessa <garciaguadalupevanessa@outlook.es>
2026-05-13 17:03:27 +0200

    Corregido error de indentación en get_all_measurements_ordered

--- repositories/sqlite_repository.py
+++ repositories/sqlite_repository.py
@@ -82,7 +82,7 @@ class SQLiteRepository:
         return self.db.query(Medicion).offset(skip).limit(limit).all()
     
     def get_all_measurements_ordered(self):
-    return (
+        return (
         self.db.query(Medicion)
         .order_by(Medicion.fecha.asc())
         .all()
